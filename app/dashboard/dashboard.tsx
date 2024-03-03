8693acc508a00b3c4352aa6e563c08c2cc4adaed
Aayush <ajoshi16@calpoly.edu>
2024-03-02 21:57:39 -0800

    Linking Sign in button to dashboard

--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-export default function PatientDashboard({ patient }) {
+export default function PatientDashboard() {
   return (
-    <div>
-      <h1>Patient Dashboard</h1>
-      <h2>{patient.name}</h2>
-      <p>Age: {patient.age}</p>
-      <p>Email: {patient.email}</p>
-      <h2>Appointments</h2>
-      {patient.appointments.map((appointment, index) => (
-        <div key={index}>
-          <p>Date: {new Date(appointment.date).toLocaleDateString()}</p>
-          <p>Doctor: {appointment.doctor}</p>
+    <div className="min-h-screen flex items-start justify-start bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl text-gray-900">
+            Patient Dashboard
+          </h2>
         </div>
-      ))}
+        <div>
+          <p className="mt-6 text-left text-gray-900">Name: Aayush Joshi</p>
+          <p className="mt-6 text-left text-gray-900">Age: 20</p>
+        </div>
+      </div>
     </div>
   );
 }
