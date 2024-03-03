13869c50a87717e9dd2cf03fd910677a9ebb03e3
Aayush <ajoshi16@calpoly.edu>
2024-03-02 21:46:35 -0800

    Dashboard first draft

---
+++ dashboard/dashboard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function PatientDashboard() {
+  return (
+    <div className="min-h-screen flex items-start justify-start bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl text-gray-900">
+            Patient Dashboard
+          </h2>
+        </div>
+        <div> 
+            <h1 className="mt-6 text-left text-gray-900">
+                Name
+            </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
