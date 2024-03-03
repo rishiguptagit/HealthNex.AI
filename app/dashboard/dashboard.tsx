465a74f73e529942414e882639835fa817a6046a
Aayush <ajoshi16@calpoly.edu>
2024-03-03 11:59:46 -0800

    Adding panel and dropdown menu for Patient Dashboard

--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -1,18 +1,68 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+
+const panelStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  height: "90px",
+  width: "100%",
+  color: "black",
+  fontSize: "250%",
+  // borderBottom: "5px solid #1060d3",
+  paddingLeft: "25px",
+  background: "linear-gradient(to right, white, #1060d3 50%)",
+};
+
+const dropDown = {
+  alignItems: "center",
+  width: "25%",
+  padding: "15px",
+  paddingTop: "8px",
+  paddingBottom: "8px",
+  fontSize: "120%",
+  border: "5px solid #ccc"
+};
+
+const options = {
+  width: "90%", 
+  fontSize: "85%",
+  borderBottom: "5px solid #ccc"
+};
 
 export default function PatientDashboard() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="min-h-screen flex items-start justify-start bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl text-gray-900">
-            Patient Dashboard
-          </h2>
-        </div>
-        <div>
-          <p className="mt-6 text-left text-gray-900">Name: Aayush Joshi</p>
-          <p className="mt-6 text-left text-gray-900">Age: 20</p>
-        </div>
+    <div className="min-h-screen bg-white text-black py-12 px-4 sm:px-6 lg:px-8">
+      <div style={panelStyle}>Patient Dashboard</div>
+
+      <div style={dropDown}
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+      >
+        <button> Appointments </button>
+        {isOpen && (
+          <div style={{paddingTop: "5%"}}>
+            <button style={options}> View Appointments </button>
+            <button style={options}> Make an Appointment </button>
+          </div>
+        )}
+      </div>
+
+      {/* {<div style={dropDown}>
+        <select>
+          <option value=""> Appointments </option>
+          <option value=""> View Appointments </option>
+          <option value=""> Make an Appointment </option>
+        </select>
+      </div>} */}
+
+      <div
+        style={{ paddingTop: "50px", paddingLeft: "20px", fontSize: "110%" }}
+      >
+        <p> First Name: FName </p>
+        <p> Last Name: Lname </p>
       </div>
     </div>
   );
