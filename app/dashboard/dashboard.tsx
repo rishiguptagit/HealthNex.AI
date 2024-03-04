e44f35969c1195cda5c231c7353c6deaf1304e13
Aayush <ajoshi16@calpoly.edu>
2024-03-03 16:15:09 -0800

    Redesigning Dashboard

--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -1,69 +1,128 @@
-"use client";
 import React, { useState } from "react";
+import Link from "next/link";
 
 const panelStyle = {
   display: "flex",
   justifyContent: "space-between",
-  alignItems: "center",
-  height: "90px",
-  width: "100%",
+  alignItens: "center",
+  height: "100%",
+  width: "100vw",
+  color: "#000066",
+  fontSize: "200%",
+};
+
+const button = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItens: "center",
+  fontWeight: "bold",
   color: "black",
-  fontSize: "250%",
-  // borderBottom: "5px solid #1060d3",
-  paddingLeft: "25px",
-  background: "linear-gradient(to right, white, #1060d3 50%)",
-};
-
-const dropDown = {
+  fontSize: "50%",
+  height: "70%",
   alignItems: "center",
-  width: "25%",
-  padding: "15px",
-  paddingTop: "7px",
-  paddingBottom: "7px",
-  fontSize: "120%",
-  border: "5px solid #ccc",
-  marginTop: "25px"
-};
-
-const options = {
-  width: "90%", 
-  fontSize: "85%",
-  borderBottom: "5px solid #ccc"
+  border: "3px solid",
+  borderRadius: "10px",
+  padding: "7px",
 };
 
 export default function PatientDashboard() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <div className="min-h-screen bg-white text-black py-12 px-4 sm:px-6 lg:px-8">
-      <div style={panelStyle}>Patient Dashboard</div>
+    <div
+      className="min-h-screen bg-white text-black py-5"
+      style={{ boxSizing: "border-box" }}
+    >
+      {/* Title */}
+      <div style={panelStyle}>
+        <div
+          style={{ display: "flex", alignItems: "center", marginLeft: "25px" }}
+        >
+          <img
+            style={{ width: "50px", height: "auto" }}
+            src="/healthnex.jpg"
+            alt="WellNex.AI"
+          />
+          <h1
+            style={{
+              fontWeight: "bold",
+              paddingLeft: "10px",
+              alignItems: "left",
+            }}
+          >
+            HealthNex.AI
+          </h1>
+        </div>
 
-      <div style={dropDown}
-        onMouseEnter={() => setIsOpen(true)}
-        onMouseLeave={() => setIsOpen(false)}
-      >
-        <button> Appointments </button>
-        {isOpen && (
-          <div style={{paddingTop: "5%"}}>
-            <button style={options}> View Appointments </button>
-            <button style={options}> Make an Appointment </button>
-          </div>
-        )}
+        {/* Log out */}
+        <Link href="/">
+          <button>
+            <div style={{ ...button, marginRight: "30px" }}>
+              <img
+                style={{ width: "20px", height: "auto", marginRight: "6px" }}
+                src="/logout.jpg"
+                alt="WellNex.AI"
+              />
+              <p>Log out</p>
+            </div>
+          </button>
+        </Link>
       </div>
 
-      {/* {<div style={dropDown}>
-        <select>
-          <option value=""> Appointments </option>
-          <option value=""> View Appointments </option>
-          <option value=""> Make an Appointment </option>
-        </select>
-      </div>} */}
+      {/* Additional Functionality */}
+      <div
+        style={{ ...panelStyle, marginTop: "20px", backgroundColor: "#e8e8e8" }}
+      >
+        <button style={{ marginLeft: "40px" }}>
+          <div style={button}>
+            <img
+              style={{
+                width: "30px",
+                height: "auto",
+              }}
+              src="/Appointment.jpg"
+              alt="WellNex.AI"
+            />
+
+            <p>Appointments and Visits</p>
+          </div>
+        </button>
+      </div>
 
       <div
-        style={{ paddingTop: "50px", paddingLeft: "20px", fontSize: "110%" }}
+        style={{
+          backgroundColor: "#1060d3",
+          width: "100vw",
+          height: "100%",
+        }}
       >
-        <p> First Name: FName </p>
-        <p> Last Name: Lname </p>
+        <p> . </p>
+      </div>
+
+      {/* Bottom */}
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          textAlign: "center",
+          fontSize: "0.875rem",
+          color: "#4A5568",
+          width: "100%",
+          paddingBottom: "1rem",
+          paddingTop: "1rem",
+          backgroundColor: "#EDF2F7",
+          height: "50xp",
+        }}
+      >
+        &copy; {new Date().getFullYear()} HealthNex.AI. All rights reserved. |{" "}
+        <Link
+          href="/privacypolicy"
+          className="text-gray-600 hover:text-navy-700"
+        >
+          Privacy Policy
+        </Link>{" "}
+        |{" "}
+        <Link href="/termsofuse" className="text-gray-600 hover:text-navy-700">
+          Terms of Use
+        </Link>
       </div>
     </div>
   );
