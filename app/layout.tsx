559e570a4335919a3d6114c1b9a5b160ddb881b1
Rishi <rishigupta.personal@gmail.com>
2024-03-21 17:10:55 -0700

    added changes for name

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "WellNex.AI",
+  title: "HealthNex.AI",
   description: "AI for healthcare",
 };
 
