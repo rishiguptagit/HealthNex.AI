ba8d6bf7c0644f500b615494d9fc47dde82fbfd0
Rishi <rishigupta.personal@gmail.com>
2024-03-21 19:20:23 -0700

    adding back

--- app/interesting_queries/interesting_queries.tsx
+++ app/interesting_queries/interesting_queries.tsx
@@ -110,7 +110,7 @@ export default function InterestingQueries() {
         </Typography.Title>
         <Link href="/">
           <Button type="link" style={{ color: "white" }}>
-            Logout
+            Back to SignIn/SignUp
           </Button>
         </Link>
       </Header>
